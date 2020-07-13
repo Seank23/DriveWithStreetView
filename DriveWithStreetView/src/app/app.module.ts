1b70a3ff8cffa9865572f0ec90dffa382303752f
Seank23 <sean.king@gmx.com>
2020-07-13 21:10:19 +0100

    Style setup

--- DriveWithStreetView/src/app/app.module.ts
+++ DriveWithStreetView/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxNavbarModule, IgxIconModule } from 'igniteui-angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      NavMenuComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      ]),
+
+      BrowserAnimationsModule,
+      IgxNavbarModule,
+      IgxIconModule,
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
